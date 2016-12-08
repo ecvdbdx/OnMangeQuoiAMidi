0b7dcbeb888ee9fd9ff7edcd7dff02dde11be3ad
Boris SCHAPIRA <borisschapira@gmail.com>
2016-12-08 14:13:59 +0100

    Adding Meal

--- src/AppBundle/Entity/Place.php
+++ src/AppBundle/Entity/Place.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use AppBundle\Entity\Meal;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -27,6 +29,62 @@ class Place
      * @ORM\Column(name="name", type="string", length=255)
      */
     private $name;
+
+    /**
+     * One Place has Many Meals.
+     *
+     * @var ArrayCollection
+     * @ORM\OneToMany(targetEntity="Meal", mappedBy="place", cascade={"persist", "remove"})
+     */
+    private $meals;
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getMeals()
+    {
+        return $this->meals;
+    }
+
+    /**
+     * @param Meal $meal
+     * @return Place
+     */
+    public function addMeal($meal)
+    {
+        if(!$this->getMeals()->contains($meal)){
+            $meal->setPlace($this);
+            $this->meals->add($meal);
+        }
+        return $this;
+    }
+
+    /**
+     * @param Meal $meal
+     * @return Place
+     */
+    public function removeMeal($meal)
+    {
+        if(!$this->getMeals()->contains($meal)){
+            $this->meals->removeElement($meal);
+        }
+        return $this;
+    }
+
+    /**
+     * @param mixed $meals
+     * @return Place
+     */
+    public function setMeals($meals)
+    {
+        $this->meals = $meals;
+        return $this;
+    }
+
+    public function __construct()
+    {
+        $this->meals = new ArrayCollection();
+    }
 
 
     /**
