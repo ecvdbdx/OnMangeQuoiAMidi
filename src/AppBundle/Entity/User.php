971938a2eb19f453591569b8751ab112b14ffeae
simon-milleto <contact@simonmilleto.com>
2017-01-05 15:38:48 +0100

    Add User entity with login

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
