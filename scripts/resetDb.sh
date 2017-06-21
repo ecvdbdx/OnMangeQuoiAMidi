6622bf64a3db3489cdc30a8a9d9a7067305526b7
Bram van Osta <bramvanosta@gmail.com>
2017-06-21 14:15:13 +0200

    [#102] :memo: Add an user account for everyone in the resetDb script

--- scripts/resetDb.sh
+++ scripts/resetDb.sh
@@ -9,4 +9,14 @@ php bin/console doctrine:schema:update --force;
 # Populate the data
 php bin/console faker:populate;
 # Create the super admin
-php bin/console fos:user:create admin admin@admin.com admin --super-admin;
+php bin/console fos:user:create admin admin@test.com admin --super-admin;
+# Create normal users
+php bin/console fos:user:create Simon simon@test.com password;
+php bin/console fos:user:create Bram bram@test.com password;
+php bin/console fos:user:create Matthieu matthieu@test.com password;
+php bin/console fos:user:create Florent florent@test.com password;
+php bin/console fos:user:create Antoine antoine@test.com password;
+php bin/console fos:user:create Guilhem guilhem@test.com password;
+php bin/console fos:user:create Guillaume guillaume@test.com password;
+php bin/console fos:user:create Clément clement@test.com password;
+php bin/console fos:user:create Romain romain@test.com password;
