8b11ba3ad8baddd6a848c917c8b99e8896302afa
Clément Ollivier <clement.o2p@gmail.com>
2017-06-21 11:27:36 +0200

    :rocket: Work on #103 - Added build task for front-end dependencies

--- scripts/fetchResetAndClearCache.sh
+++ scripts/fetchResetAndClearCache.sh
@@ -3,6 +3,8 @@
 git fetch origin;
 git reset origin/master --hard;
 composer install -o;
+npm i;
+npm run build;
 php bin/console cache:clear --env=prod;
 ./scripts/resetDb.sh
 ./scripts/phpmetrics.sh
