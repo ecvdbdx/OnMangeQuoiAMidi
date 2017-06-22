ec8915ba42f80f5a0999707f3be0571d49029609
Clément Ollivier <clement.o2p@gmail.com>
2017-06-22 16:04:51 +0200

    Remove forgotten ref. to shell script

--- scripts/fetchResetAndClearCache.sh
+++ scripts/fetchResetAndClearCache.sh
@@ -4,7 +4,6 @@ git fetch origin;
 git reset origin/master --hard;
 composer install -o;
 php bin/console cache:clear --env=prod;
-./scripts/printLatestBuildDate.sh
 ./scripts/installDevelopment.sh
 ./scripts/phpmetrics.sh
 
