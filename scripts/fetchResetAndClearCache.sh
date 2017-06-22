ab7aca24d22bec3974186e2adf058510ed68173e
Clément Ollivier <clement.o2p@gmail.com>
2017-06-22 13:15:37 +0200

    Modified path in sed command

--- scripts/fetchResetAndClearCache.sh
+++ scripts/fetchResetAndClearCache.sh
@@ -5,7 +5,7 @@ git reset origin/master --hard;
 composer install -o;
 php bin/console cache:clear --env=prod;
 latest_commit=$(git log -1 --format=%cd)
-sed -i "" "s/\${latest_commit}/${latest_commit}/" ./app/Resources/views/base.html.twig
+sed -i "" "s/\${latest_commit}/${latest_commit}/" app/Resources/views/base.html.twig
 ./scripts/installDevelopment.sh
 ./scripts/phpmetrics.sh
 
