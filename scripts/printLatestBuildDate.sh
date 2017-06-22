49afc59878a8017763ac984b3d9109a23e9991a4
Clément Ollivier <clement.o2p@gmail.com>
2017-06-22 15:00:46 +0200

    Enhancement on last build date printing script

--- scripts/printLatestBuildDate.sh
+++ scripts/printLatestBuildDate.sh
@@ -1,4 +1,4 @@
 latest_commit=$(git log -1 --format=%cd);
 sed -i.tmp "s/\${latest_commit}/$latest_commit/" ./app/Resources/views/base.html.twig
-mv ./app/Resources/views/base.html.twig.tmp ./app/Resources/views/base.html.twig
+sed -i "s/\+0200//" ./app/Resources/views/base.html.twig
 rm -rf ./app/Resources/views/base.html.twig.tmp
