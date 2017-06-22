98f5eace9d247165ae2253b948104cbd3d0e1dac
Clément Ollivier <clement.o2p@gmail.com>
2017-06-22 11:14:21 +0200

    Add current year and last build date in footer

--- scripts/fetchResetAndClearCache.sh
+++ scripts/fetchResetAndClearCache.sh
@@ -4,6 +4,8 @@ git fetch origin;
 git reset origin/master --hard;
 composer install -o;
 php bin/console cache:clear --env=prod;
+latest_commit=$(git log -1 --format=%cd)
+sed -i "" "s/\${latest_commit}/${latest_commit}/" ./app/Resources/views/base.html.twig
 ./scripts/installDevelopment.sh
 ./scripts/phpmetrics.sh
 
